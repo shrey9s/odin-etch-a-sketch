e28a05fb7eabbbcfa7456077dbb173316d10f35a
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-14 17:37:31 +1300

    Modify createGrid function and use flexbox to form grid
    createGrid was not implemented properly the first time, and so did not
    form a 16x16 grid. Modified it so that a "row" div is created first and
    each "square" div is appended to it. This created 16 "rows" with 16 "squares"/
    "columns", which could be arranged into a 16x16 grid using flexbox.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Create 16x16 grid of square divs
-
 function createGrid(numRows, numColumns) {
     for (let i = 0; i < numRows; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
         for (let j = 0; j < numColumns; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
-            gridContainer.appendChild(square);
+            //square.textContent = `(${i}, ${j})`;
+            row.appendChild(square);
         }
+        gridContainer.appendChild(row);
     }
 }
 
