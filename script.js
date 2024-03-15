0fda969e5564446177eeedcb19162c4741899f37
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-15 22:52:59 +1300

    Create addMousedownEvent and addMouseoverEvent functions
    When the grid is resized, the NodeList gridSquares is changed and so
    the mousedown and mouseover event listeners attached to it are destroyed
    and do not respond. Have to update gridSquares and add new event listeners
    to it every time a new grid is generated.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function createGrid(numRows = 16, numColumns = numRows) {
-    console.log(numRows, numColumns)
     for (let i = 0; i < numRows; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -12,35 +11,10 @@ function createGrid(numRows = 16, numColumns = numRows) {
     }
 }
 
-const gridContainer = document.querySelector(".grid-container");
-createGrid(16, 16);
-
 function changeColour(square) {
     const randomColour = "#" + (Math.floor(Math.random() * 16777215)).toString(16);    // from CSS-Tricks
     square.style.backgroundColor = randomColour;
 }
-
-const gridSquares = document.querySelectorAll(".square");
-let sketchStarted = false;
-
-gridSquares.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-        if (sketchStarted) {
-            sketchStarted = false;  // stop sketch
-        } else {
-            sketchStarted = true;
-            changeColour(square);  // start sketch
-        }
-    });
-});
-
-gridSquares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        if (sketchStarted && !square.style.backgroundColor) {
-            changeColour(square);
-        }
-    });
-});
 
 function setGridSize(userInput) {
     let gridValues = (userInput.split("x")).map((n) => parseInt(n));  // if separator "x" not in string, a one-element array with the original string is returned
@@ -49,11 +23,11 @@ function setGridSize(userInput) {
     while (numRows > 100 || numColumns > 100) {  // undefined > 100 = false
         userInput = prompt("The number of rows/columns have to be <= 100! Please choose again.");
         if (!userInput) createGrid();  // if user presses cancel or returns "" the default 16x16 grid is generated (default values for numRows & numColumns)
-        gridValues = (userInput.split("x")).map((n) => parseInt(n));
+        gridValues = (userInput.split("x")).map((n) => parseInt(n)); // if userInput = null; ERROR
         numRows = gridValues[0];
         numColumns = gridValues[1];
     }
-    createGrid(numRows, numColumns);  
+    createGrid(numRows, numColumns);
 }
 // if user enters only one value (e.g., 45), numRows = 45 and undefined is passed for numColumns,
 // so numColumns is initialised with the default value -> numColumns = numRows = 45.
@@ -65,13 +39,53 @@ function removeGrid() {
     });
 }
 
+const gridContainer = document.querySelector(".grid-container");
+let gridSquares = document.querySelectorAll(".square");
+let sketchStarted = false;
+
+createGrid();
+addMousedownEvent();
+addMouseoverEvent();
+
+function addMousedownEvent() {
+    gridSquares = document.querySelectorAll(".square");  // get current NodeList of .square divs (in case they have been replaced when grid size is changed)
+    gridSquares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (sketchStarted) {
+                sketchStarted = false;  // stop sketch
+            } else {
+                sketchStarted = true;
+                changeColour(square);  // start sketch
+            }
+        });
+    });
+}
+
+function addMouseoverEvent() {
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            if (sketchStarted && !square.style.backgroundColor) {
+                changeColour(square);
+            }
+        });
+    });
+}
+
 const gridSizeBtn = document.querySelector(".btn");
 gridSizeBtn.addEventListener("click", () => {
-    const input = prompt("Choose a grid size (e.g., 20x20, 45)", "16x16");
+    const input = prompt("Choose a grid size (e.g., 30, 20x20, 20x30)", "16x16");
     if (!input) return;
     removeGrid();
     setGridSize(input);
+    addMousedownEvent();  // updates gridSquares NodeList
+    addMouseoverEvent();  // has to come after so it uses updated gridSquares NodeList
 });
+
+
+
+
+
+
 
 
 
